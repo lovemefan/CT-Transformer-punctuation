0f909ea3e99e817f59423c25b7d2402d35f415aa
lovemefan <lovemefan@outlook.com>
2023-04-19 10:12:06 +0800

    add entry point of ctt-punctuator

---
+++ ctt-punctuator.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+# @FileName  :ctt-punctuator.py
+# @Time      :2023/4/13 15:03
+# @Author    :lovemefan
+# @Email     :lovemefan@outlook.com
+
+
+__author__ = "lovemefan"
+__copyright__ = "Copyright (C) 2023 lovemefan"
+__license__ = "MIT"
+__version__ = "v0.0.1"
